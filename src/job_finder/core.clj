2f0440b70dc6c13c3deab8b0c23f91b1d666b543
James Vickers <jamesvickers19@gmail.com>
2019-09-08 10:04:01 -0600

    Comment, finish up, output files

--- src/job_finder/core.clj
+++ src/job_finder/core.clj
@@ -1,8 +1,7 @@
 (ns job-finder.core)
-  ;(:import [org.jsoup Jsoup]))
-
 
 (defn get-page
+  "Gets a page"
   [address]
   (if (nil? address)
     nil
@@ -15,31 +14,40 @@
           nil)))))
 
 (defn get-link [^org.jsoup.nodes.Element item]
+  "Gets the 'href' of a HTML element"
   (.attr item "href"))
 
-(defn get-company-link
+(defn get-clojure-company-link
+  "Extracts a clojure company link, drilling down into the HTML to get it"
   [^org.jsoup.nodes.Element item]
   (let [item_p (.get (.getElementsByTag item "p") 0)
         a (.get (.getElementsByTag item_p "a") 0)]
     (get-link a)))
 
-(defn get-company-list
+(defn get-clojure-company-list
+  "Extracts the URL's of the companies using Clojure from the community page"
   []
   (let [page (get-page "https://clojure.org/community/companies")
         list-items (.getElementsByTag page "li")]
-    (map get-company-link list-items)))
+    (map get-clojure-company-link list-items)))
 
 (defn contains-any-substring?
+  "Returns true if s contains a string in substrings, false otherwise."
   [s substrings]
   (some true? (map #(.contains (.toLowerCase s) (.toLowerCase %)) substrings)))
 
 (defn make-absolute
+  "Makes a full URL given a base-url and endpoint.  endpoint may be a full URL
+  itself; if it doesn't start with / it's assumed to be as such and is just returned."
   [base-url endpoint]
   (if (.startsWith endpoint "/")
     (str (.replaceAll base-url "/$" "") endpoint)
     endpoint))
 
 (defn find-job-link
+  "Finds the 'jobs' link given a page and it's URL.
+  Returns nil if one can't be found, commonly because the page source uses
+  Javascript to generate HTML (this program only deals in straight HTML)"
   [page url]
   (let [search-terms #{"jobs" "career" "work for us" "work with us" "join us"}
         search-term-matches #(contains-any-substring? % search-terms)
@@ -53,6 +61,10 @@
 
 
 (defn search-jobs
+  "Given company-urls, accesses the site and tries to find the 'jobs' page
+  and whether it has remote jobs.  Returns a map with keys
+  [:no-company-page :no-job-page :job-page :job-page-remote]
+  to vectors that contain URL's - either elements from company-urls or discovered job pages."
   [company-urls]
   (reduce
     (fn [m url]
@@ -71,6 +83,16 @@
      :job-page-remote []}
     company-urls))
 
-; Call search-jobs and write each key into a text file
+(defn write-map-to-files!
+  "Takes a map presumed to have collection values
+  and writes a file named as each key with one line
+  per element in the value collection."
+  [m]
+  (doall
+    (map (fn [[k v]] (spit (str (name k) ".txt")
+                           (clojure.string/join "\n" v)))
+         m)))
 
-; figure out how to open browser with list of pages (like in a text file)
+; Example usage:
+(comment
+  (write-map-to-files! (search-jobs (get-clojure-company-list))))
