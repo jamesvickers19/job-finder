6fa23bb218aad966934f994deba35c1d0522a691
James Vickers <jamesvickers19@gmail.com>
2024-04-13 11:30:14 -0600

    Update for clojure companies list no longer having links
    Try building that company url by it's name or search google
    and open the first result for the company careers.

--- src/job_finder/core.clj
+++ src/job_finder/core.clj
@@ -1,35 +1,107 @@
-(ns job-finder.core)
+(ns job-finder.core
+  (:require [clojure.string :as string])
+  (:import (java.net URLEncoder)
+           (java.nio.charset StandardCharsets)))
 
 (defn get-page
   "Gets a page"
   [address]
-  (if (nil? address)
-    nil
-    (do
-      (println "get page: " address)
-      (try
-        (.get (org.jsoup.Jsoup/connect address))
-        (catch Exception e
-          (println "Link" address "failed:" (.getMessage e))
-          nil)))))
+  (when address
+    (println "get page: " address)
+    (try
+      (-> (org.jsoup.Jsoup/connect address)
+          (.userAgent "ExampleBot 1.0 (+http://botexample.com/)")
+          (.followRedirects true)
+          .get)
+      (catch Exception e
+        (println "Link" address "failed:" (.getMessage e))
+        nil))))
 
-(defn get-link [^org.jsoup.nodes.Element item]
+(defn get-link
   "Gets the 'href' of a HTML element"
+  [^org.jsoup.nodes.Element item]
   (.attr item "href"))
 
-(defn get-clojure-company-link
-  "Extracts a clojure company link, drilling down into the HTML to get it"
-  [^org.jsoup.nodes.Element item]
-  (let [item_p (.get (.getElementsByTag item "p") 0)
-        a (.get (.getElementsByTag item_p "a") 0)]
-    (get-link a)))
+(defn with-dot-com
+  [s]
+  (str s ".com"))
+
+(defn with-https
+  [domain]
+  (str "https://" domain))
+
+(defn remove-spaces
+  [s]
+  (.replace s " " ""))
+
+(defn- old-new-company-names
+  [company-name-with-acquired-by]
+  (let [[old new] (.split company-name-with-acquired-by "\\(acquired by")]
+    [(with-dot-com old)
+     (-> new (.split "\\)") first with-dot-com)]))
+
+(defn company-desc->urls
+  [company-desc]
+  (when company-desc
+    (let [company-name (.toLowerCase company-desc)
+          domains (cond
+                    ; if name has a dot, just use the name
+                    (.contains company-name ".") [company-name]
+                    ; if it says 'acquired by', try old and new company name
+                    (.contains company-name "(acquired by") (old-new-company-names company-name)
+                    ; if name has a parenthesis, take everything before it
+                    (.contains company-name "(") [(-> company-name
+                                                      (.split "\\(")
+                                                      first
+                                                      with-dot-com)]
+                    ; if name has slash, try names before and after it
+                    (.contains company-name "/") (mapv with-dot-com (.split company-name "/"))
+                    :else [(with-dot-com company-name)])]
+      (map (comp with-https remove-spaces) domains))))
+
+(comment
+  (map company-desc->urls ["LonoCloud (acquired by ViaSat)"
+                           "8th Light"
+                           "Iris.tv"
+                           "Marktbauer/Comida da gente"]))
+
+(defn url-encode
+  [s]
+  (URLEncoder/encode s StandardCharsets/UTF_8))
+
+(defn find-company-url-via-search
+  "Tries to find company homepage by opening the first search result for them"
+  [company-desc]
+  (let [results-page (->> (str company-desc " careers")
+                          url-encode
+                          (str "https://www.google.com/search?btnI=I'm+Feeling+Lucky&q=")
+                          get-page)
+        company-url (some-> results-page
+                            (.select "a")
+                            first
+                            get-link)]
+    (when (and company-url (.contains company-url "http"))
+      company-url)))
+
+(defn find-company-urls
+  [company-desc]
+  (or (->> company-desc
+           company-desc->urls
+           (filter get-page)
+           not-empty)
+      [(find-company-url-via-search company-desc)]))
 
 (defn get-clojure-company-list
   "Extracts the URL's of the companies using Clojure from the community page"
   []
   (let [page (get-page "https://clojure.org/community/companies")
-        list-items (.getElementsByTag page "li")]
-    (map get-clojure-company-link list-items)))
+        company-list-items (.getElementsByTag page "li")]
+    (->> company-list-items
+         (take 5)
+         (map #(.text %))
+         (map find-company-urls)
+         flatten
+         (filter some?))))
 
 (defn contains-any-substring?
   "Returns true if s contains a string in substrings, false otherwise."
@@ -49,7 +121,7 @@
   Returns nil if one can't be found, commonly because the page source uses
   Javascript to generate HTML (this program only deals in straight HTML)"
   [page url]
-  (let [search-terms #{"jobs" "career" "work for us" "work with us" "join us"}
+  (let [search-terms #{"jobs" "career" "work for us" "work with us" "join us" "openings"}
         search-term-matches #(contains-any-substring? % search-terms)
         links (.getElementsByTag page "a")
         is-jobs? #(or (search-term-matches (.toLowerCase (get-link %)))
@@ -67,21 +139,21 @@
   to vectors that contain URL's - either elements from company-urls or discovered job pages."
   [company-urls]
   (reduce
-    (fn [m url]
-      (if-let [page (get-page url)]
-          (let [job-link (find-job-link page url)]
-            (if-let [job-link-page (get-page job-link)]
-              (let [remote? (contains-any-substring? (.. job-link-page text toLowerCase)
-                                                     #{"remote" "work from home" "anywhere" "any location"})
-                    k (if remote? :job-page-remote :job-page)]
-                (update m k #(conj % job-link)))
-              (update m :no-job-page #(conj % url))))
-          (update m :no-company-page #(conj % url))))
-    {:no-company-page []
-     :no-job-page []
-     :job-page []
-     :job-page-remote []}
-    company-urls))
+   (fn [m url]
+     (if-let [page (get-page url)]
+       (let [job-link (find-job-link page url)]
+         (if-let [job-link-page (get-page job-link)]
+           (let [remote? (contains-any-substring? (.. job-link-page text toLowerCase)
+                                                  #{"remote" "work from home" "anywhere" "any location"})
+                 k (if remote? :job-page-remote :job-page)]
+             (update m k #(conj % job-link)))
+           (update m :no-job-page #(conj % url))))
+       (update m :no-company-page #(conj % url))))
+   {:no-company-page []
+    :no-job-page []
+    :job-page []
+    :job-page-remote []}
+   company-urls))
 
 (defn write-map-to-files!
   "Takes a map presumed to have collection values
@@ -89,10 +161,12 @@
   per element in the value collection."
   [m]
   (doall
-    (map (fn [[k v]] (spit (str (name k) ".txt")
-                           (clojure.string/join "\n" v)))
-         m)))
+   (map (fn [[k v]] (spit (str (name k) ".txt")
+                          (string/join "\n" v)))
+        m)))
 
 ; Example usage:
 (comment
-  (write-map-to-files! (search-jobs (get-clojure-company-list))))
+  (->> (get-clojure-company-list)
+       search-jobs
+       write-map-to-files!))
